ced31a24f88770a105eb7253dc96cbc78c5b2dba
Vedant Patil <vedant.waman.patil@gmail.com>
2026-01-14 18:21:58 -0500

    Started implementing telnet. Finished initilization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[derive(Debug)]
+struct Url {
+    scheme: String,
+    host: String,
+    path: String,
+}
+
+impl Url {
+    fn new(input: &str) -> Self {
+        let (scheme, rest) = input.split_once("://").unwrap_or(("", input));
+        let (host, path) = rest.split_once('/').unwrap_or((rest, ""));
+        let parsed = Self {
+            scheme: scheme.to_string(),
+            host: host.to_string(),
+            path: format!("/{}", path),
+        };
+
+        assert_eq!(parsed.scheme, "http");
+        parsed
+    }
+}
+
 fn main() {
-    println!("Hello, web browser engine");
+    let url = Url::new("http://www.youtube.com/");
+    println!("{:?}", url);
 }
