5c2d04b09a16233150cbfe6624b0536b89ee5be7
Vedant Patil <vedant.waman.patil@gmail.com>
2026-01-19 17:17:48 -0500

    Basic connection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use std::net::TcpStream;
+
+#[derive(Debug, Clone)]
 struct Url {
     scheme: String,
     host: String,
@@ -20,7 +22,20 @@ impl Url {
     }
 }
 
+// Originally was going to do a one to one converstion but ran into issues with iternet protocols
+fn request(url: &Url) -> std::io::Result<TcpStream> {
+    let port = ":80";
+
+    let address = url.host.clone() + port;
+
+    let stream = TcpStream::connect(address)?;
+
+    Ok(stream)
+}
+
 fn main() {
     let url = Url::new("http://www.youtube.com/");
+    let request = request(&url);
     println!("{:?}", url);
+    println!("{:?}", request);
 }
