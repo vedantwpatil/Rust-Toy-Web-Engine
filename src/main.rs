3ee26021d910defff02141614c5335f7de0a7371
Vedant Patil <vedant.waman.patil@gmail.com>
2026-01-22 17:03:37 -0500

    Print the html body as well

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env::args;
+use std::io::Result;
 use std::io::{BufRead, BufReader, Read, Write};
 use std::net::TcpStream;
 
@@ -21,58 +23,79 @@ impl Url {
         assert_eq!(parsed.scheme, "http");
         parsed
     }
-}
 
-// Originally was going to do a one to one converstion but ran into issues with internet protocols
-// so there are slight modifications
-//
-// Plan to do a second pass where I make the code more rustic
-fn request(url: &Url) -> std::io::Result<TcpStream> {
-    let port = ":80";
+    // Originally was going to do a one to one converstion but ran into issues with internet protocols
+    // so there are slight modifications
 
-    let address = url.host.clone() + port;
+    // TODO:
+    // Plan to do a second pass where I make the code more rustic
+    fn request(url: &Url) -> Result<TcpStream> {
+        let port = ":80";
+        let address = url.host.clone() + port;
+        let mut stream = TcpStream::connect(address)?;
 
-    let mut stream = TcpStream::connect(address)?;
-
-    let request = format!(
-        "GET /ip HTTP/1.0\r\n\
+        let request = format!(
+            "GET /ip HTTP/1.0\r\n\
          Host: {}\r\n\
          \r\n",
-        url.host
-    );
+            url.host
+        );
 
-    stream.write_all(request.as_bytes())?;
+        stream.write_all(request.as_bytes())?;
 
-    let mut reader = BufReader::new(&stream);
-    let mut line = String::new();
+        let mut reader = BufReader::new(stream);
+        let mut line = String::new();
 
-    reader.read_line(&mut line)?;
-    println!("Status: {}", line.trim());
-    line.clear();
-
-    loop {
         reader.read_line(&mut line)?;
-        if line == "\r\n" {
-            break;
-        }
-        if let Some((key, value)) = line.split_once(":") {
-            println!("Key: {}\nValue: {}", key.trim(), value.trim());
-        }
+        println!("Status: {}", line.trim());
         line.clear();
+
+        loop {
+            reader.read_line(&mut line)?;
+            if line == "\r\n" {
+                break;
+            }
+            if let Some((key, value)) = line.split_once(":") {
+                println!("Key: {}\nValue: {}", key.trim(), value.trim());
+            }
+            line.clear();
+        }
+
+        Ok(reader.into_inner())
     }
-
-    let mut response = String::new();
-
-    // Moved the reading of the stream to the bufReader
-    reader.read_to_string(&mut response)?;
-    println!("{}", response);
-
-    Ok(stream)
 }
 
-fn main() {
-    let url = Url::new("http://www.google.com/");
-    let request = request(&url);
-    println!("{:?}", url);
-    println!("{:?}", request);
+fn show(stream: TcpStream) -> Result<()> {
+    let mut body = String::new();
+    let mut reader = BufReader::new(stream);
+
+    reader.read_to_string(&mut body)?;
+    println!("HTML Body: {}", body);
+
+    Ok(())
+}
+
+fn load(url: Url) -> Result<()> {
+    let body = Url::request(&url)?;
+    show(body)?;
+
+    Ok(())
+}
+fn main() -> Result<()> {
+    // Earlier previous debug/testing lines
+    // let url = Url::new("http://www.google.com/");
+    // let request = Url::request(&url);
+    // println!("{:?}", url);
+    // println!("{:?}\n", request);
+
+    let args: Vec<String> = args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <url>", args[0]);
+        return Ok(());
+    }
+
+    load(Url::new(&args[1]))?;
+
+    Ok(())
 }
