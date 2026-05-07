03892e3dae55b73fa135f5b00d95b95fe6215dd9
Vedant Patil <vedant.waman.patil@gmail.com>
2026-05-06 22:28:44 -0400

    Started the html parser for the python implementation

--- python/ex.py
+++ python/ex.py
@@ -96,8 +96,66 @@ class Browser:
 
 
 class Text:
-    def __init__(self, text):
+    def __init__(self, text, parent):
         self.text = text
+        self.children = []
+        self.parent = parent
+
+
+class Element:
+    def __init__(self, tag, parent) -> None:
+        self.tag = tag
+        self.children = []
+        self.parent = parent
+
+
+class HTMLParser:
+    def __init__(self, body) -> None:
+        self.body = body
+        self.unfinished = []
+
+    def add_text(self, text):
+        parent = self.unfinished[-1]
+        node = Text(text, parent)
+        parent.children.append(node)
+
+    def add_tag(self, tag):
+        if tag.startswith("/"):
+            if len(self.unfinished) == 1:
+                return
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        else:
+            parent = self.unfinished[-1] if self.unfinished else None
+            node = Element(tag, parent)
+            self.unfinished.append(node)
+
+    def finish(self):
+        while len(self.unfinished) > 1:
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        return self.unfinished.pop()
+
+    def parse(self):
+        text = ""
+        in_tag = False
+        for c in self.body:
+            if c == "<":
+                in_tag = True
+                if text:
+                    self.add_text(text)
+                text = ""
+            elif c == ">":
+                in_tag = False
+                self.add_tag(text)
+                text = ""
+            else:
+                text += c
+        if not in_tag and text:
+            self.add_text(text)
+        return self.finish()
 
 
 class Tag:
@@ -136,6 +194,7 @@ def lex(body):
 
 FONTS = {}
 
+
 def get_font(size, weight, style):
     key = (size, weight, style)
     if key not in FONTS:
@@ -162,15 +221,24 @@ class Layout:
         if isinstance(tok, Text):
             for word in tok.text.split():
                 self.word(word)
-        elif tok.tag == "i":      self.style = "italic"
-        elif tok.tag == "/i":     self.style = "roman"
-        elif tok.tag == "b":      self.weight = "bold"
-        elif tok.tag == "/b":     self.weight = "normal"
-        elif tok.tag == "small":  self.size -= 2
-        elif tok.tag == "/small": self.size += 2
-        elif tok.tag == "big":    self.size += 4
-        elif tok.tag == "/big":   self.size -= 4
-        elif tok.tag == "br":     self.flush()
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
         elif tok.tag == "/p":
             self.flush()
             self.cursor_y += VSTEP
